77e42e1dc03d7534d7416cd39a1d8e520ed561da
heckman-codes <liamheckmancode@gmail.com>
2020-01-11 20:26:11 -0800

    trying different binding

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "localhost",
+    host: "0.0.0.0",
     port: 3306,
     user: "root",
     password: "password",
