5560da6ae2e5a6cd24aca5a0a6ee49f10d860099
heckman-codes <liamheckmancode@gmail.com>
2020-01-12 13:10:17 -0800

    attempting to update dependencies

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-    host: "0.0.0.0",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
