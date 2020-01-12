959c2e8ea38a4740ac81edb5def55028a6d52e5d
heckman-codes <liamheckmancode@gmail.com>
2020-01-11 20:14:27 -0800

    trouble shooting heroku deployment

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var exphbs = require("express-handlebars");
-var routes = require("./controllers/burger_controllers");
+var routes = require("controllers/burger_controllers");
 var app = express();
 var PORT = process.env.PORT || 3000
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("./public"));
+app.use(express.static("public"));
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
